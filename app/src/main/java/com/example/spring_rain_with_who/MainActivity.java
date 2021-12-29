cf6964503af27befd675232b4b3019f50958b452
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-12-29 12:57:47 +0900

    initial

---
+++ app/src/main/java/com/example/spring_rain_with_who/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.spring_rain_with_who;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
