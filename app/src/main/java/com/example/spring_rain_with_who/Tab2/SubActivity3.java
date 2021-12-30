d8ff5fc8806f538e7ad15c342884e682df3f0680
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-12-30 15:47:02 +0900

    Edit SubActivity3.java, tab2_view3.xml to make album3

--- app/src/main/java/com/example/spring_rain_with_who/Tab2/SubActivity3.java
+++ app/src/main/java/com/example/spring_rain_with_who/Tab2/SubActivity3.java
@@ -1,4 +1,19 @@
 package com.example.spring_rain_with_who.Tab2;
 
-public class SubActivity3 {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spring_rain_with_who.R;
+
+public class SubActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab2_view2);
+    }
+
 }
