72cc83fb2ba9d4e3c2b5aabda32a86f6be74b337
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-12-30 15:41:04 +0900

    Edit SubActivity1.java, tab2_view1.xml to make album1

--- app/src/main/java/com/example/spring_rain_with_who/Tab2/SubActivity1.java
+++ app/src/main/java/com/example/spring_rain_with_who/Tab2/SubActivity1.java
@@ -1,4 +1,27 @@
 package com.example.spring_rain_with_who.Tab2;
 
-public class SubActivity1 {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spring_rain_with_who.R;
+
+public class SubActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab2_view1);
+    }
+
 }
