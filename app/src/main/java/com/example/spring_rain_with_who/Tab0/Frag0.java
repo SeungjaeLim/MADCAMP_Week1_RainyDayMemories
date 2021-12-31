5bbe996cb5a6ca6d50e4495372a29e3be37df96e
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-12-31 15:18:50 +0900

    Implement Temp, Weather in Tap 0 with openweather API

--- app/src/main/java/com/example/spring_rain_with_who/Tab0/Frag0.java
+++ app/src/main/java/com/example/spring_rain_with_who/Tab0/Frag0.java
@@ -1,5 +1,6 @@
 package com.example.spring_rain_with_who.Tab0;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,7 +10,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.spring_rain_with_who.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +28,13 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +45,12 @@ public class Frag0 extends Fragment implements OnMapReadyCallback {
 
     private MapView mapView = null;
 
+    TextView weatherView;
+    TextView tempView;
+
+    static RequestQueue requestQueue;
+
+
     public Frag0()
     {
 
@@ -34,6 +58,9 @@ public class Frag0 extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        }
         super.onCreate(savedInstanceState);
     }
 
@@ -42,11 +69,64 @@ public class Frag0 extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.tab0_main, container, false);
+        ImageButton button = layout.findViewById(R.id.imageButton);
+        weatherView = layout.findViewById(R.id.weatherView);
+        tempView = layout.findViewById(R.id.tempView);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CurrentWeatherCall();
+            }
+        });
 
         mapView = (MapView) layout.findViewById(R.id.map);
         mapView.getMapAsync(this);
 
         return layout;
+    }
+
+    private void CurrentWeatherCall(){
+        String apikey = "cafd431dd6e47ced531ef159c7432e07";
+        String city = "daejeon";
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=cafd431dd6e47ced531ef159c7432e07";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    JSONArray weatherJson = jsonObject.getJSONArray("weather");
+                    JSONObject weatherObj = weatherJson.getJSONObject(0);
+                    String weather = weatherObj.getString("description");
+                    weatherView.setText(weather);
+
+                    JSONObject tempK = new JSONObject(jsonObject.getString("main"));
+                    double tempDo = (Math.round((tempK.getDouble("temp")-273.15)*100)/100.0);
+                    tempView.setText(tempDo +  "°C");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                return params;
+            }
+
+        };
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
     }
 
     @Override
