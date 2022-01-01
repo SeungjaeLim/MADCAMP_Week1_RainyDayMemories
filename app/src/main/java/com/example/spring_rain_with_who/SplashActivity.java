cad2d87e43f341ff043ba73254cabbdd287f9bec
SeungjaeLim <seungjaelim@kaist.ac.kr>
2022-01-01 17:53:37 +0900

    Make Lottie Splash

--- app/src/main/java/com/example/spring_rain_with_who/SplashActivity.java
+++ app/src/main/java/com/example/spring_rain_with_who/SplashActivity.java
@@ -1,17 +1,54 @@
 package com.example.spring_rain_with_who;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class SplashActivity  extends AppCompatActivity {
+
+    LottieAnimationView animationView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceStare) {
         super.onCreate(savedInstanceStare);
         setContentView(R.layout.activity_splash);
+
+        textView = findViewById(R.id.textView);
+        animationView = findViewById(R.id.lottie);
+        animationView.setAnimation("loading.json");
+        animationView.playAnimation();
+        animationView.setRepeatCount(2);
+
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animationView.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
